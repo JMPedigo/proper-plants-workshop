830652bdc55d1e1800659b50ecedaf11102e6efa
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-13 17:11:16 -0500

    Adds removeFromCart and addToCart to CartItemQuantity, & imports CartItemQuantity

--- src/cart/CartItem.jsx
+++ src/cart/CartItem.jsx
@@ -1,13 +1,19 @@
+import CartItemQuantity from "./CartItemQuantity";
+
 /** I need a component to populate a list item with name, image, and quantity, with two
  *  buttons that add to cart or remove from cart */
 
-export default function CartItem({ item, quantity }) {
+export default function CartItem({ item, removeFromCart, addToCart }) {
   return (
     <li className="cart-item">
       <div>
         {item.image} {item.name}
       </div>
-      <CartItemQuantity />
+      <CartItemQuantity
+        item={item}
+        removeFromCart={removeFromCart}
+        addToCart={addToCart}
+      />
     </li>
   );
 }
--- src/cart/CartItemQuantity.jsx
+++ src/cart/CartItemQuantity.jsx
@@ -4,6 +4,7 @@ export default function CartItemQuantity({ item, removeFromCart, addToCart }) {
   return (
     <div className="cart-item-quantity">
       <button onClick={() => removeFromCart(item)}>-</button>
+      <span>{item.quantity}</span>
       <button onClick={() => addToCart(item)}>+</button>
     </div>
   );
