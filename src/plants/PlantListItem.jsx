bae0965974e715b6fe865d54ae82444b4d9648bc
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-13 16:19:31 -0500

    Changes <h2> to <h3>

--- src/plants/PlantListItem.jsx
+++ src/plants/PlantListItem.jsx
@@ -2,7 +2,7 @@ export default function PlantListItem({ plant }) {
   return (
     <li className="plant">
       <figure>{plant.image}</figure>
-      <h2>{plant.name}</h2>
+      <h3>{plant.name}</h3>
       <button onClick={() => addToCart(plant)}></button>
     </li>
   );
